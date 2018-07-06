87fd706943463044460b7e8105a8e2a8429e57b2
Allen <pazmnew@163.com>
2018-07-06 08:16:40 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Auth::routes();
 
 // 资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+// 话题骨架资源路由
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
