ef1709e915ca755829e01c1d286d224181c58879
Allen <pazmnew@163.com>
2018-07-08 09:46:50 +0000

    自动翻译slug

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,12 @@ Auth::routes();
 // 资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 // 话题骨架资源路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 // 分类下的话题列表资源路由
 Route::resource('categories', 'CategoriesController', ['only'=>['show']]);
 
 // 上传图片路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
