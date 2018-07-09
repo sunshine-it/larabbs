d944ada5747a7678ed144a876f2837394c111ad7
Allen <pazmnew@163.com>
2018-07-09 10:07:12 +0000

    用户可以发表回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -6,16 +6,18 @@ use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+// 模型监控器类
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        // 话题下每新增一条回复，此处应该 +1
+        $reply->topic->increment('reply_count', 1);
     }
 
-    public function updating(Reply $reply)
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
