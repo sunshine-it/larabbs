8d39fcca2358700e8813e0c8f68a22a1a6ce2405
Allen <pazmnew@163.com>
2018-07-11 09:12:21 +0000

    fix

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -26,9 +26,11 @@ class ReplyObserver
     }
 
     // 减去话题回复数/使用deleted() 方法来监控『回复删除后』事件：
+    // 当回复被删除后，话题的 reply_count 也需要 -1
     public function deleted(Reply $reply)
     {
-        // 当回复被删除后，话题的 reply_count 也需要 -1
-        $reply->topic->decrement('reply_count', 1);
+        if ($reply->topic->reply_count > 0) {
+            $reply->topic->decrement('reply_count', 1);
+        }
     }
 }
